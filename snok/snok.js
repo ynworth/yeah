f9da36552d2b3617be50bd5c8cb87fa85f3c54a8
ynworth <yournotworth@gmail.com>
2024-11-28 14:58:26 +0200

    init

---
+++ snok/snok.js
@@ -0,0 +1,290 @@
+let snakeTiles = [ //Pieces of the snok [X,Y],...
+    [7, 5],[7, 6],[7, 7]
+]
+let apples = [
+    [9, 9]
+] //the apples!!!!!!!!!!!!!!
+let tiles = [];
+
+let oldTiles = [];
+
+let gameOver = false;
+const invincibilityTotal = 1;
+let invincibility = invincibilityTotal;
+
+
+const removableClasses = [
+    "apple","snake",
+    "snake-head","snake-body-vertical","snake-body-horizontal",
+    "snake-body-top-right","snake-body-top-left","snake-body-bottom-right","snake-body-bottom-left",
+    "snake-head-right","snake-head-left","snake-head-up","snake-head-down",
+    "snake-tail-right","snake-tail-left","snake-tail-up","snake-tail-down",
+];
+
+let gameSizeX = 15;
+let gameSizeY = 15;
+let gameSizeTotal = gameSizeX * gameSizeY;
+
+let tummy = 0;
+
+let snakeDir = [1, 0];
+let originalInput = [1, 0]
+
+function findRandomPos(){//find a random empty tile for apple to spawn on
+    if (snakeTiles.length >= tiles.length){//if snake covers the screen
+        console.log("u win!!");
+        //REFRESH PAGE HERE
+        alert("victory")
+        location.reload();
+        return null;
+    }
+
+    while (true) { 
+        let randomNum = Math.floor((Math.random()*tiles.length) + 1);
+        let tile = tiles[randomNum];
+        
+        let vec2 = [tile.dataset.x, tile.dataset.y]
+        let tileX = vec2[0];
+        let tileY = vec2[1];
+        
+        
+
+        if (tile != null && !tile.classList.contains("snake") && !tile.classList.contains("apple")){
+            return vec2
+        }
+    }
+}
+
+function die(){
+    if (invincibility > 0){
+        invincibility--
+    }else{
+        gameOver = true
+        alert("game over");
+        setTimeout(1000);
+        location.reload();
+    }
+}
+
+function tick(){
+    originalInput = snakeDir;
+
+    let snakeHead = snakeTiles[0];//snakeTiles.length-1s
+
+    let newVec2 = [
+        (snakeHead[0]+snakeDir[0]),
+        (snakeHead[1]+snakeDir[1])
+    ];
+
+    
+    let tileX = newVec2[0];
+    let tileY = newVec2[1];
+
+    let tile = document.querySelector(`[data-x="${tileX}"][data-y="${tileY}"]`);
+
+    let didDamage = false;
+
+    if (tile == null){
+        didDamage = true;
+        die();
+    }
+    if (tile.classList.contains("snake")){
+        didDamage = true;
+        die();
+    }
+
+    if (tile.classList.contains("apple")){
+        console.log("YUMMY!");
+        apples.splice(apples.indexOf(newVec2), 1);
+        apples.push(findRandomPos());
+        tummy += 1;
+    }
+
+    if (didDamage == false){
+        invincibility = invincibilityTotal;
+
+        if (tummy > 0){
+            tummy -= 1;
+        }
+        else {
+            snakeTiles.pop();
+        }
+
+        snakeTiles.unshift(newVec2);
+    }
+    
+}
+
+document.addEventListener('keydown', (event) => {
+    
+    if (event.key.toLowerCase() === 'w' && !(originalInput[1] == 1)) {
+        snakeDir = [0, -1];
+    } else if (event.key.toLowerCase() === 's' && !(originalInput[1] == -1)) {
+        snakeDir = [0, 1];
+    } else if (event.key.toLowerCase() === 'a' && !(originalInput[0] == 1)) {
+        snakeDir = [-1, 0];
+    } else if (event.key.toLowerCase() === 'd' && !(originalInput[0] == -1)) {
+        snakeDir = [1, 0];
+    }
+});
+
+function displayThings(){
+
+    //Return old snok and apples to normal tiles
+
+    oldTiles.forEach(function(vec2){
+        let tileX = vec2[0];
+        let tileY = vec2[1];
+        
+        let tile = document.querySelector(`[data-x="${tileX}"][data-y="${tileY}"]`);
+
+        removableClasses.forEach(function(string){
+            if (tile.classList.contains(string)){
+                tile.classList.remove(string);
+            }
+        })
+        
+        //turn tile back to normal
+    });
+
+    //DisplaySnok
+    snakeTiles.forEach((vec2) =>{
+        let tileX = vec2[0];
+        let tileY = vec2[1];
+
+        let id = tileX+(tileY*gameSizeY);
+        let tile = document.querySelector(`[data-x="${tileX}"][data-y="${tileY}"]`);
+
+        let i = snakeTiles.indexOf(vec2);
+
+        let getSnakeTile = function(tilePos, useCorners){
+            
+            if (useCorners) {
+                let tileTypeBack = getSnakeTile(snakeTiles[i+1], false);
+                if ((tileTypeBack) === undefined){return getSnakeTile(snakeTiles[i-1])}
+                let tileTypeFront = getSnakeTile(snakeTiles[i-1], false);
+                if ((tileTypeFront) === undefined){return getSnakeTile(snakeTiles[i+1])}
+                
+                if ((tileTypeFront == 0 && tileTypeBack == 2) || (tileTypeBack == 0 && tileTypeFront == 2)) {
+                    return 7;
+                }
+                else if ((tileTypeFront == 1 && tileTypeBack == 2) || (tileTypeBack == 1 && tileTypeFront == 2)) {
+                    return 6;
+                }
+                else if ((tileTypeFront == 0 && tileTypeBack == 3) || (tileTypeBack == 0 && tileTypeFront == 3)) {
+                    return 5;
+                }
+                else if ((tileTypeFront == 1 && tileTypeBack == 3) || (tileTypeBack == 1 && tileTypeFront == 3)) {
+                    return 4;
+                }
+                else {
+                    return tileTypeBack;
+                }
+            }
+            else {
+                if ((tilePos) === undefined){return undefined; }
+
+                if (tileX-tilePos[0] > 0) {
+                    return 0; // right
+                }
+                else if (tileX-tilePos[0] < 0) {
+                    return 1; // left
+                }
+                else if (tileY-tilePos[1] < 0) {
+                    return 2; // up
+                }
+                else if (tileY-tilePos[1] > 0) {
+                    return 3; // down
+                }
+                
+            }
+        }
+
+        tile.classList.add("snake");
+        
+        let tileType = 0;
+        let icons
+
+        if (i === 0) {// head
+            tileType = getSnakeTile(snakeTiles[i+1], false);
+            icons = [
+                "snake-head-right", // right
+                "snake-head-left", // left
+                "snake-head-up", // up
+                "snake-head-down", // down
+            ];
+        }
+        else { // body
+            tileType = getSnakeTile(snakeTiles[i], true);
+            if (i === snakeTiles.length-1){
+                icons = [
+                    "snake-tail-left", // right
+                    "snake-tail-right", // left
+                    "snake-tail-down", // up
+                    "snake-tail-up", // down
+                ];
+                
+            }else{
+                icons = [
+                    "snake-body-horizontal", // right
+                    "snake-body-horizontal", // left
+                    "snake-body-vertical", // up
+                    "snake-body-vertical", // down
+                    "snake-body-top-right", // top-right
+                    "snake-body-top-left", // top-left
+                    "snake-body-bottom-right", // bottom-right
+                    "snake-body-bottom-left", // bottom-left
+                ];
+            }
+            
+        }
+        console.log(tileType)
+        tile.classList.add(icons[tileType]);
+
+        oldTiles.push([tileX, tileY]);
+        //change the tile to a snake here!!
+    });
+
+    //Display apples
+    apples.forEach(function(vec2){
+        let tileX = vec2[0];
+        let tileY = vec2[1];
+
+        let id = tileX+(tileY*gameSizeY);
+        let tile = document.querySelector(`[data-x="${tileX}"][data-y="${tileY}"]`);
+
+        tile.classList.add("apple");
+
+        oldTiles.push([tileX, tileY]);
+        //change the tile to a apple.
+    });
+}
+
+
+function spawnTiles(){
+    const container = document.querySelector('.container');
+
+    for (let x = 1, y = 1, loop = 1;  loop <= gameSizeTotal; loop++) {
+        let tile = document.createElement('div');
+       
+        tile.classList.add("tile");
+        tile.id = loop;
+        tile.dataset.x = x;
+        tile.dataset.y = y;
+        container.appendChild(tile);
+        tiles.push(tile);
+        x++;
+        x > gameSizeX ? (x = 1, y++) : null;
+    }
+}
+
+spawnTiles();
+
+function gameLoop() {
+    if (gameOver == false){
+        tick();
+        displayThings();
+    }
+}
+setInterval(gameLoop, 100);
+
---
+++ sweepminer/ms.js
@@ -0,0 +1,298 @@
+const btns = [];
+
+let gameSizeX = 25;
+document.documentElement.style.setProperty('--game_size_x', gameSizeX);
+let gameSizeY = 25;
+let gameSizeTotal = gameSizeX*gameSizeY;
+let currentClicked = null;
+let flagStartCell = null;
+let firstClick = true;
+
+
+function updateSize() {
+    const swag = document.getElementById('swag').value;
+    gameSizeX = parseInt(swag, 10) || 25;    
+    gameSizeY = gameSizeX;
+    gameSizeTotal = gameSizeX * gameSizeY;
+
+    document.documentElement.style.setProperty('--game_size_x', gameSizeX);
+    resetGame();
+}
+// updateSize();
+document.getElementById('swag').addEventListener('input', (bomb) => {
+    updateSize();
+});
+
+document.getElementById('swagger').addEventListener('input', (bomb) => {
+    updateSize();
+});
+
+document.getElementById('swaggiest').addEventListener('mousedown', (event) => {
+    animateText();
+    firstClick = true;
+    resetGame();
+});
+
+function animateText() {
+    const text = document.getElementById("animatedText");
+    
+    text.style.fontSize = "20px";
+    text.style.opacity = "1";
+    
+    setTimeout(() => {
+        text.style.transition = "none";
+        text.style.fontSize = "20px";
+        text.style.opacity = "1";
+
+        setTimeout(() => {
+            text.style.transition = "font-size 1s ease, opacity 1s ease";
+            text.style.fontSize = "500px"; // change size here
+            text.style.opacity = "0";
+        }, 50);
+    }, 50);
+}
+
+function resetGame() {
+    const mines = document.getElementById('swagger').value;
+    clearTiles();
+    spawnTiles();
+    placeMines(mines/100);
+}
+
+
+function clearTiles() {
+    const getBtns = document.querySelectorAll('.btn');
+    for (let btn of getBtns) {
+        btn.remove();
+    }
+}
+
+function showMines(){
+    const getBtns = document.querySelectorAll('.btn');
+    for (let btn of getBtns) {
+        if (btn.dataset.isMine === 'true') {
+            btn.classList.add('boom');
+            btn.disabled = true;
+        }
+    }
+}
+
+document.addEventListener('mouseup', (event) =>{
+    if (currentClicked && !currentClicked.disabled) {
+        currentClicked.classList.remove('clicked');
+        currentClicked = null;
+    }
+});
+
+function mobileLongPress(btn) {
+}
+
+function spawnTiles() {
+    const container = document.querySelector('.container');
+    for (let x = 1, y = 1, loop = 1; loop <= gameSizeTotal; loop++) {
+        const btn = document.createElement('button');
+        btn.dataset.x = x;
+        btn.dataset.y = y;
+        btn.classList.add('btn', 'btnClosed');
+        btn.id = loop;
+     
+        const duration = 200;
+        let longPress;
+        btn.addEventListener('touchstart', (event) => {
+            longPress = setTimeout(() => {
+                btn.classList.toggle('flag');
+            }, duration);
+        });
+        btn.addEventListener('touchend', () => clearTimeout(longPress));
+        btn.addEventListener('touchmove', () => clearTimeout(longPress));
+        btn.addEventListener('touchcancel', () => clearTimeout(longPress));
+
+        btn.addEventListener('contextmenu', (event) => { 
+            event.preventDefault();
+            if (!btn.disabled && event.button === 2 && flagStartCell === btn) {
+                btn.classList.toggle('flag');
+                btn.classList.remove('clicked');
+            }
+        });
+        
+        btn.addEventListener('mousedown', (event) =>{
+            if (!btn.disabled && (event.button === 0 || event.button === 2)) {
+                flagStartCell = btn;
+                btn.classList.add('clicked');
+                currentClicked = btn;
+            }
+        });
+
+        btn.addEventListener('mouseleave', () => {
+            if (currentClicked === btn) {
+                btn.classList.remove('clicked');
+                currentClicked = null;
+            }
+        })
+        
+        btn.addEventListener('click', (event) => { 
+            console.log(event.button);
+            if (btn.classList.contains('flag')) {
+                return;
+            }
+
+            if (firstClick === true){
+                firstClick = false;
+                if (btn.dataset.isMine === 'true'){
+                    delete btn.dataset.isMine;
+                }
+            }
+
+            if (countAdjMines(btn) === 0) {
+                console.log("YES IT IS EMPTY!");
+                clearAdj(btn);
+             }
+
+            btn.disabled = true;
+
+            if (btn.dataset.isMine === 'true') {
+                showMines();
+                setTimeout(() => alert('You lost! You should swag yourself now!'), 1000);
+            }
+            else {
+                const mineCount =  countAdjMines(btn);
+                const classToAdd = getClassMineCount(mineCount);
+                if (classToAdd) {
+                    btn.classList.add(classToAdd);
+                }
+            }
+            checkWin();
+        });
+
+        container.appendChild(btn);
+        btns.push(btn);
+        x++;
+        // if (x === 10) { x = 1; y++; } // fuck this line!!
+        x > gameSizeX ? (x = 1, y++) : null;
+    }
+}
+
+function placeMines(minePercent) {
+    const mines = Math.round(gameSizeTotal*minePercent); // this was math.floor first, i don't know why it bothered me so much to change it to math.round
+    const selectedButtons = new Set();
+
+    while (selectedButtons.size < mines) {
+        const randMineId = Math.floor((Math.random() * gameSizeTotal) + 1);
+        const randBtn = document.getElementById(randMineId);
+        if (!selectedButtons.has(randMineId)) {
+            selectedButtons.add(randBtn);
+            // selectedButtons.add(numUnderBtn);
+            // numUnderBtn.dataset.isMine = 'true';
+            randBtn.dataset.isMine = 'true';
+            // if(numUnderBtn) {
+            //     numUnderBtn.classList.add('one');
+            // }
+        }
+        // console.log(randBtn);
+        // console.log(randMine.dataset.x, randMine.dataset.y);
+        // console.log(document.querySelector(`[data-x="${randMine.dataset.x}"][data-y="${randMine.dataset.y}"][data-is-bomb="true"]`));
+    }
+}
+
+updateSize();
+
+
+function isOnScreen(x, y) {
+    return x >= 1 && x <= gameSizeX && y >= 1 && y <= gameSizeY;
+}
+
+function getAdj(btn) {
+    const x = Number(btn.dataset.x);
+    const y = Number(btn.dataset.y);
+
+    const adj = [
+        [x, y],            // middle
+        [x + 1, y],        // right
+        [x - 1, y],        // left
+        [x, y + 1],        // bottom
+        [x, y - 1],        // top
+        [x + 1, y + 1],    // bottom right
+        [x - 1, y - 1],    // top left
+        [x + 1, y - 1],    // top right
+        [x - 1, y + 1]     // bottom left
+    ];
+    return adj // connected with the code below
+        .filter(([x, y]) => isOnScreen(x, y))
+        .map(([x, y]) => document.querySelector(`[data-x="${x}"][data-y="${y}"]`))  // get every element
+        .filter(cell => cell !== null); // that the cell isn't nothing
+}
+function clearAdj(btn) {
+    const adj = getAdj(btn);
+    for (let item of adj) {
+        if (!item.disabled){
+            item.disabled = true;
+            item.classList.add("empty");
+        
+            const mineCount =  countAdjMines(item);
+            const classToAdd = getClassMineCount(mineCount);
+            if (classToAdd) {
+                item.classList.add(classToAdd);
+            }
+            
+            if (mineCount === 0) {
+                clearAdj(item);
+            }
+        }
+    }
+}
+
+function countAdjMines(btn) {
+    const adj = getAdj(btn);
+    return adj.filter(adj => adj.dataset.isMine === 'true').length;
+}
+
+function getClassMineCount(count) {
+    switch (count) {
+        case 1: return 'one';
+        case 2: return 'two';
+        case 3: return 'three';
+        case 4: return 'four';
+        case 5: return 'five';
+        case 6: return 'six';
+        case 7: return 'seven';
+        case 8: return 'eight';
+        default: return '';
+    }
+}
+
+function checkWin() {
+    const btns = document.querySelectorAll('.btn');
+    let hasWon = true;
+    for (let btn of btns) {
+        if (!btn.disabled && btn.dataset.isMine !== 'true') {
+            hasWon = false;
+            break;
+        }
+    }
+    if (hasWon) {
+        setTimeout(() => alert('You won! (Swagging yourself still recommended)'), 1000);
+    }
+}
+
+// function revealCell(btn) {
+//     btn.disabled = true;
+//     const mineCount = coundAdjMines(btn);
+//     const classToAdd 
+// }
+// function getAdj(btn) {
+//     const id = Number(btn.id);
+//     const x = Number(btn.dataset.x);
+//     const y = Number(btn.dataset.y);
+//     const adj = [
+//         btn,
+//         x < 9 ? document.querySelector(`[data-x="${x+1}"][data-y="${y}"]`) : null, // right
+//         x > 1 ? document.querySelector(`[data-x="${x-1}"][data-y="${y}"]`) : null, // left
+//         y < 9 ? document.querySelector(`[data-x="${x}"][data-y="${y+1}"]`) : null, // bottom
+//         x > 1 ?document.querySelector(`[data-x="${x}"][data-y="${y-1}"]`) : null, // top
+//         x < 9 && y < 9 ? document.querySelector(`[data-x="${x+1}"][data-y="${y+1}"]`) : null, // bottom left
+//         x < 1 && y < 1 ? document.querySelector(`[data-x="${x-1}"][data-y="${y-1}"]`) : null, // top right
+//         x < 1 && y < 9 ? document.querySelector(`[data-x="${x-1}"][data-y="${y+1}"]`) : null, // bottom right 
+//         x < 9 && y < 1 ? document.querySelector(`[data-x="${x+1}"][data-y="${y-1}"]`) : null // top left
+//     ]
+//     return adj.filter(adj => adj !== null);
+//     }
